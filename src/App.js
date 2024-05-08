ef97c211ef29ba3af737402266af3b4522e8552b
Hariom Chouhan <75517242+hariomchouhan@users.noreply.github.com>
2024-05-08 12:39:29 +0000

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main class="flex justify-center gap-4 flex-col min-h-screen">
+      <h1 class="text-9xl text-center font-bold">Hari ॐ </h1>
+    </main>
+  );
+}
+
+export default App;
