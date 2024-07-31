00f1aa972bade718cacdfecee655a65704407c68
Hariom Chouhan <75517242+hariomchouhan@users.noreply.github.com>
2024-07-31 08:55:23 +0000

    Understand the useState concept of value and banch task in fibre

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-
+import "./App.css"
 function App() {
   const [counter, setCounter] = useState(15);
 
   const addValue = () => {
     if (counter < 20) {
-      setCounter(counter + 1);
-      console.log(counter);
+      setCounter(prevCounter => prevCounter + 1);
+      setCounter(prevCounter => prevCounter + 1);
+      setCounter(prevCounter => prevCounter + 1);
+      setCounter(prevCounter => prevCounter + 1);
     }
   };
 
